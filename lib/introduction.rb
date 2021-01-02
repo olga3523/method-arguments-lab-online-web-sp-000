920b81dc9d3ed81259e50aad6b4f862e50849313
olga gomelskiy <olichka23.85@gmail.com>
2021-01-02 00:55:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,10 @@
 
+def introduction_with_language(name="")
+  puts "Hi, my name is #{name}."
+
+end
+
+
 def introduction_with_language(name="Dan",language="Ember.js")
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 
